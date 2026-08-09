403f38dd071915b52c7bbdc641bc66eaab971718
FMEDIAG <ecorreofgo@gmail.com>
2026-08-09 12:38:13 +0200

    Eliminar index.tsx

--- src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { ValuationWorkspace } from "@/components/valuation/ValuationWorkspace";
-
-export const Route = createFileRoute("/")({
-  component: ValuationWorkspace,
-});
