9a4ec39e5b1fe62d4ad064152634986c9b2b8d70
FMEDIAG <ecorreofgo@gmail.com>
2026-08-15 13:50:13 +0200

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   tanstackStart: {
     server: { entry: "server" },
   },
 });
+
 
